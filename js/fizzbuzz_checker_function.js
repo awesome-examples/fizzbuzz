c0602c86a601531d53174792aaf38a46ac290192
James Cahill <jcahill773@gmail.com>
2018-10-27 14:39:02 +0100

    Update and rename fizzbuzzChecker.js to fizzbuzz_checker_function.js

--- js/fizzbuzz_checker_function.js
+++ js/fizzbuzz_checker_function.js
@@ -1,9 +1,10 @@
 const fizzbuzzChecker = value => {
-  const fizz = (value % 3 === 0) ? 'fizz' : '';
-  const buzz = (value % 5 === 0) ? 'buzz' : '';
+  const fizz = (value % 3 === 0) ? 'Fizz' : '';
+  const buzz = (value % 5 === 0) ? 'Buzz' : '';
   const nan = (fizz === '') ? (buzz === '') ? value : '' : '';
   const res = fizz+buzz+nan;
   return res;
 }
 
-console.log(fizzbuzzChecker(2));
+// Example Usage
+// fizzbuzzChecker(number));

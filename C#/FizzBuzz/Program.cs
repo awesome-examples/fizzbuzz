91200803b8195bf4508795da5118f6108d5c38e5
Milan Tenk <tenkmilan@gmail.com>
2018-10-27 13:07:25 +0200

    C# implementation added

---
+++ C#/FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                if (fizz && buzz)
+                    Console.WriteLine("FizzBuzz");
+                else if (fizz)
+                    Console.WriteLine("Fizz");
+                else if (buzz)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}

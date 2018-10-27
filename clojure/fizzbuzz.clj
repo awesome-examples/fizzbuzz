48f7469b0e6dad57ab373d2511bf795651fa872f
Kelvin Mai <kelvin.mai002@gmail.com>
2018-10-27 17:54:38 -0400

    Added clojure example

---
+++ clojure/fizzbuzz.clj
@@ -0,0 +1,15 @@
+(ns clojure.fizzbuzz)
+
+;; fizzbuzz function
+(def fizz? #(zero? (mod % 3)))
+(def buzz? #(zero? (mod % 5)))
+(def fizzbuzz? #(and (fizz? %) (buzz? %)))
+(defn fizzbuzz
+  [x]
+  (cond
+    (fizzbuzz? x) "FizzBuzz"
+    (fizz? x) "Fizz"
+    (buzz? x) "Buzz"))
+
+;; fizzbuzz over an list from 1 to 100
+(map fizzbuzz (range 1 100))

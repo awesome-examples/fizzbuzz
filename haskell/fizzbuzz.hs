6fbd5d8995d643e7024858a2670798eceafd68c3
MrHanSan <hans.sandbu@protonmail.com>
2018-10-27 16:25:40 +0200

    Updated fizzbuzz

--- haskell/fizzbuzz.hs
+++ haskell/fizzbuzz.hs
@@ -1,8 +1,9 @@
 fizzbuzz :: [String]
-fizzbuzz = [if n `mod` 15 == 0 then "FizzBuzz" else
+fizzbuzz = [if n `mod` 3 == 0 && n `mod` 5 == 0 then "FizzBuzz" else
             if n `mod` 3  == 0 then "Fizz" else
             if n `mod` 5  == 0 then "Buzz" else
             show n | n <- [1..100]]
+
 
 main :: IO ()
 main =  mapM_ putStrLn fizzbuzz

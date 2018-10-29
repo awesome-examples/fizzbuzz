9fd761e1af17b1319c1d0f4f945dd0d3c4cc4a97
Deepak Duggirala <deepak.duggirala@target.com>
2018-10-29 10:48:23 +0530

    FizzBuzz in elm

---
+++ elm/src/Main.elm
@@ -0,0 +1,54 @@
+module Main exposing (Msg(..), main, update, view)
+
+import Browser
+import Html exposing (Html, div, text)
+
+
+main =
+    Browser.sandbox
+        { init =
+            { count = 100
+            }
+        , update = update
+        , view = view
+        }
+
+
+type alias Model =
+    { count : Int }
+
+
+type Msg
+    = Null
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+view : Model -> Html Msg
+view model =
+    div [] <|
+        List.concat <|
+            List.map (htmlLine << fizzbuzz) (List.range 1 model.count)
+
+
+fizzbuzz : Int -> String
+fizzbuzz x =
+    if modBy 3 x == 0 then
+        "Fizz"
+
+    else if modBy 5 x == 0 then
+        "Buzz"
+
+    else if modBy 3 x == 0 && modBy 5 x == 0 then
+        "FizzBuzz"
+
+    else
+        String.fromInt x
+
+
+htmlLine : String -> List (Html Msg)
+htmlLine s =
+    [ text s, Html.br [] [] ]

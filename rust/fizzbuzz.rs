cfde0a5918b441d84daf5ba46cc7993cb17dbd0d
Georgi Kirilov <gkkirilov@gmail.com>
2018-10-29 11:46:34 +0200

    Add Rust implementation

---
+++ rust/fizzbuzz.rs
@@ -0,0 +1,13 @@
+fn main() {
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+}

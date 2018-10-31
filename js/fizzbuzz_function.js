eb2b5d6d8d91a78ab1e4590b85c4a383c607f60d
James Cahill <jcahill773@gmail.com>
2018-10-31 18:41:06 +0000

    Update and rename fizzbuzz_checker.js to fizzbuzz_function.js

--- js/fizzbuzz_function.js
+++ js/fizzbuzz_function.js
@@ -1,15 +1,15 @@
-function fizzbuzzChecker() {
+function fizzbuzz() {
     for (i = 1; i < 101; i++) {
         if (i % 15 == 0) {
-            console.log("fizzbuzz");
+            console.log("FizzBuzz");
         } else if (i % 3 == 0) {
-            console.log("fizz");
+            console.log("Fizz");
         } else if (i % 5 == 0) {
-            console.log("buzz");
+            console.log("Buzz");
         } else {
             console.log(i);
         }
     }
 }
 
-fizzbuzzChecker();
+fizzbuzz();
